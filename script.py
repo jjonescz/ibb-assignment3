76a6ff06797b59115e9e57a30e7592d2720f039f
Jan Joneš <jjones@outlook.cz>
2021-01-13 19:42:01 +0100

    Load image paths

--- script.py
+++ script.py
@@ -2,6 +2,7 @@
 import os
 import json
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import tensorflow as tf
 import tensorflow_addons as tfa
@@ -19,5 +20,15 @@ EPOCHS = 35
 EXP_ID = "initial"  # subfolder inside `out/` with saved weights
 TRAIN = False  # `True` = train, `False` = load saved checkpoints
 OUT_DIR = os.path.join("out", EXP_ID)
+
+# %%
+# Load image paths.
+translations = pd.read_csv('awe-translation.csv')
+images = dict()
+labels = dict()
+for dataset in ['train', 'test']:
+    rows = translations['AWE-Full image path'].str.startswith(dataset)
+    images[dataset] = list(translations[rows]['AWE image path'])
+    labels[dataset] = list(translations[rows]['Subject ID'])
 
 # %%
