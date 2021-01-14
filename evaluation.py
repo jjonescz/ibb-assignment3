e3f2224e10bb3ce56f38e630522382206b237a34
Jan Joneš <jjones@outlook.cz>
2021-01-14 22:46:20 +0100

    Specify Python dependencies

--- evaluation.py
+++ evaluation.py
@@ -1,12 +1,8 @@
 # %%
 import os
-import shutil
 import pickle
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
-import tensorflow as tf
-import tensorflow_addons as tfa
 
 # %%
 # Initialize.
--- script.py
+++ script.py
@@ -6,7 +6,6 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import tensorflow as tf
-import tensorflow_addons as tfa
 
 # %%
 # Parameters
